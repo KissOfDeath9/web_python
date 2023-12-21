bd876a42a752ad34b634a9b6137fd839183b3779
Nazar <118357068+KissOfDeath9@users.noreply.github.com>
2023-12-21 03:23:16 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,40 @@
 from flask import Flask
-from flask_bcrypt import Bcrypt
-from flask_login import LoginManager
-import os
+from .extensions import db, migrate, bcrypt, login_manager,jwt
+from config import config
 
 app = Flask(__name__)
-app.secret_key = b"secret"
 
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
-login_manager.login_message_category = 'warning'
 
-from app import views
-from app import database
+def create_app(config_name='default'):
+    app = Flask(__name__)
+    app.config.from_object(config.get(config_name))
+
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+    login_manager.init_app(app)
+    bcrypt.init_app(app)
+
+  
+
+    with app.app_context():
+        from .home import home_blueprint
+        app.register_blueprint(home_blueprint, url_prefix='/')
+
+        from .auth import auth_blueprint
+        app.register_blueprint(auth_blueprint, url_prefix='/auth')
+
+        from .account import account_blueprint
+        app.register_blueprint(account_blueprint, url_prefix='/account')
+
+        from .todo import todo_blueprint
+        app.register_blueprint(todo_blueprint, url_prefix='/todo')
+
+        from .feedback import feedback_blueprint
+        app.register_blueprint(feedback_blueprint, url_prefix='/feedback')
+
+        from .cookies import cookies_blueprint
+        app.register_blueprint(cookies_blueprint, url_prefix='/cookies')
+
+
+        return app
