1c0b4eb66b4146b7738884eed8291ca9dfc86824
Nazar <118357068+KissOfDeath9@users.noreply.github.com>
2023-12-21 03:33:50 +0200

    Update config.py

---
+++ config.py
@@ -0,0 +1,27 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    DEBUG = False
+    DEVELOPMENT = False
+    SECRET_KEY = b"secret"
+    FLASK_SECRET = SECRET_KEY
+
+class DevConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+    WTF_CSRF_ENABLED = True
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///flasktododb.db")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+class ProdConfig(Config):
+    WTF_CSRF_ENABLED = True
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///flasktododb.db")
+
+
+
+config = {
+    'dev': DevConfig,
+    'prod': ProdConfig,
+    'default': DevConfig,
+   
+}
